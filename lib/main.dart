2ef55d3f891ec62eade162354d1d151064f28b6f
Bluearcher101 <ramisabbagh01@gmail.com>
2024-07-15 17:37:30 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+void main () {
+  
+// for (var = value ; condition ; operator ) { code }
+
+int year ;
+
+for (year = 1900; year <= 2020 ; year++) {
+  if (year ==1950) {
+    print ("year ===== 1950") ;
+  } 
+  print (year) ;
+}
+
+
+}
