9d1de3525b3744a6ef5d549c91f3a4c201bc21e6
Bluearcher101 <ramisabbagh01@gmail.com>
2024-08-01 09:23:41 -0400

    .map method

--- lib/map_method44.dart
+++ lib/map_method44.dart
@@ -1,5 +1,20 @@
 void main (){
   List names = ["rami" , "norpek" , "osama" , "lorax"];
+  //.map is used here to return a value chosen
+
    
-   
+   var lengthElement = names.map((e) {
+    if(e == "rami") {
+      return "reemi";
+    }
+    return "no";
+   });
+   print(lengthElement);
+
+   Map info = { "name" : "rami" , "age" : 23 , "phone" : 81954639457};
+   List infokeys = info.entries.map((e) => e.key).toList();
+
+   print(infokeys);
+   //here we used .entries to be able to extract the keys out of the map
+   //the same thing cab be done with values of the map
 }
