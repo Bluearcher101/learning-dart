52cb1e99b4a612cc2823691bcd0e623a49b6ce25
Bluearcher101 <ramisabbagh01@gmail.com>
2024-08-01 10:25:00 -0400

    O.O.P

---
+++ lib/oop47.dart
@@ -0,0 +1,28 @@
+//O.O.P is an abrieveition of Object Orientation Program
+//OOP is used to organize your work
+//it can be found in all languaging
+//it is expressed using class
+void main(){
+//here we will make a prototype (instance)
+Mobile info = new Mobile();
+//here we declared a variable as the information in the class Mobile as "info"
+
+
+print(info.name);
+print(info.RAM);
+//this way you can print anything you want and mention first the variable in the void main then mention the variable you want to print in the class
+ 
+
+}
+
+/*you can put any information you want inside it like
+var = 
+function
+constructor
+set and get */
+
+class Mobile {
+  String name = "xiomi 12 T Pro";
+  double inches = 7.2;
+  String RAM = "12G";
+}
