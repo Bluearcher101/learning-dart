d7600dfa14faaeafda894cbb63910cc28da0de86
Bluearcher101 <ramisabbagh01@gmail.com>
2024-08-01 10:03:31 -0400

    shorthand if

---
+++ lib/shorthand_if46.dart
@@ -0,0 +1,15 @@
+void main(){
+   //shorthand if is a way to use the if function in a way that is brief
+   int a = 32;
+
+   a > 30 ? print("a > 30 ") : print("a < 30");
+   //after putting the ? you put the function that will activate if the condition is true
+   // the function you put after : is the one that will activate if the condition is false
+
+   //there more additions to the if condition you can put like ||(or) &&(and) for example
+   String name = "rami";
+   name = "sabbagh";
+
+   name == "rami" || name == "sabbagh" ? print("my name is rami sabbagh") : print("that is not my name");
+
+}
